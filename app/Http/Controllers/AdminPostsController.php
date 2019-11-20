c0596fd64c16d196524f8d732d3f9f57d4df08b3
xinru <s3a632065@student.ncut.edu.tw>
2019-11-20 15:50:51 +0800

    練習2-3 編輯 PostsController 裡設定 type­-hinting (修改)

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Http\PostRequests;
+use App\Http\Requests\PostRequest;
 use App\Post;
 
 class AdminPostsController extends Controller
